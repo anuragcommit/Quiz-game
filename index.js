7cc41f0bc551e78323b940eb51cddbe29c974a9c
Anuragbbd <anuragguru69@gmail.com>
2026-06-27 00:10:36 +0530

    added ques and button logic

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const btn = document.querySelector("#start_btn");
 const h1 = document.querySelector("h1");
-const start_screen = document.querySelector("#start_screen");
-const ques_screen = document.querySelector("#ques_screen");
+const startscreen = document.querySelector("#start_screen");
+const questionscreen = document.querySelector(".ques_screen");
+
+
+const quizQuestions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            {text: "London", correct: false},
+            {text: "Berlin", correct: false},
+            {text: "Paris", correct: true},
+            {text: "Madrid", correct: false},
+        ],
+
+},
+{
+     question: "What is the capital of France?",
+        answers: [
+            {text: "London", correct: false},
+            {text: "Berlin", correct: false},
+            {text: "Paris", correct: true},
+            {text: "Madrid", correct: false},
+        ],
+},
+{
+     question: "What is the capital of France?",
+        answers: [
+            {text: "London", correct: false},
+            {text: "Berlin", correct: false},
+            {text: "Paris", correct: true},
+            {text: "Madrid", correct: false},
+        ],
+},
+{
+     question: "What is the capital of France?",
+        answers: [
+            {text: "London", correct: false},
+            {text: "Berlin", correct: false},
+            {text: "Paris", correct: true},
+            {text: "Madrid", correct: false},
+        ],
+}
+]
 
 
 
-
-
-btn.addEventListener("click", ()=>{
-    start_screen.style.display = "none";
-    ques_screen.classList.remove("screen");
+btn.addEventListener("click", () =>{
+    startscreen.classList.add('screen');
+        console.log(startscreen.className);
+    questionscreen.classList.remove('screen');
 })
